03ddc61ab5c9570c079a3becbb2df822df7f79fa
akrsh24 <akrsh24@gmail.com>
2021-07-14 13:33:15 +0530

    Added layout changes

---
+++ image-gallery-app/components/Layout.js
@@ -0,0 +1,78 @@
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const LayoutContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  box-shadow: 5px 10px 18px #888888;
+  margin: ${(props) => (props.viewType === 'list' ? '0 30%' : '0 15%')};
+`;
+
+const LayoutNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const LayoutMain = styled.main`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LayoutFooter = styled.footer`
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+  }
+`;
+
+export default function Layout({
+  title,
+  keywords,
+  content,
+  description,
+  children,
+  viewType,
+}) {
+  return (
+    <LayoutContainer viewType={viewType}>
+      <Head>
+        <title>{title}</title>
+        <meta name={description} content={content} />
+        <meta name="keywords" content={keywords} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <LayoutNav>
+        <h1>Pexels Image Gallery</h1>
+      </LayoutNav>
+      <LayoutMain>{children}</LayoutMain>
+      <LayoutFooter>
+        <a
+          href="https://www.pexels.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Pexels
+        </a>
+      </LayoutFooter>
+    </LayoutContainer>
+  );
+}
--- image-gallery-app/pages/_app.js
+++ image-gallery-app/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyles = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
-export default MyApp
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
