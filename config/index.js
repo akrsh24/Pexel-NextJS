d9c6be66d7433b3ae9a59c8f31046ad24db612b9
akrsh24 <akrsh24@gmail.com>
2021-07-18 21:53:42 +0530

    Updated URL

--- config/index.js
+++ config/index.js
@@ -6,6 +6,6 @@ export const environment = {
     API_URL: 'http://localhost:3000',
   },
   production: {
-    API_URL: 'pexel-next-js-fy63abn1e-akrsh24.vercel.app',
+    API_URL: 'https://pexel-next-js-fy63abn1e-akrsh24.vercel.app',
   },
 };
